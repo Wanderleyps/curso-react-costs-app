aa4ef58fff64e72be8af287fa6d0c11e9de90213
Wanderleyps <wanderleyps794@gmail.com>
2024-05-16 23:02:15 -0300

    create base layout

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,7 +1,29 @@
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Container from './components/layout/Container';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <p>Costs</p>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
