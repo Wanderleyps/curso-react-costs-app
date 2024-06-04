190136fcecfa0e61c737cbee597068beb8450327
Wanderleyps <wanderleyps794@gmail.com>
2024-06-04 20:16:08 -0300

    using json-server to mock back api

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -11,7 +11,11 @@ function Select({ text, name, options, handleOnChange, value }) {
         value={value || ''}
       >
         <option>Selecione uma opção</option>
-        
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}      
+          </option>
+        ))}        
       </select>
     </div>
   )
