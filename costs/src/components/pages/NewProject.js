80dd753aa8a57b0433cb3bcb54cfcac26e477a65
Wanderleyps <wanderleyps794@gmail.com>
2024-06-24 22:28:40 -0300

    add feedback message component

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import ProjectForm from '../project/ProjectForm';
-import styles from './NewProject.module.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
 
 function NewProject() {
-    
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function createPost(project) {
-        // initialize costs and services
-        project.cost = 0
-        project.services = []
+  function createPost(project) {
+    // initialize costs and services
+    project.cost = 0;
+    project.services = [];
 
-        fetch('http://localhost:5000/projects', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(project)
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            navigate('/projects', { message: 'Projeto criado com sucesso!'})
-        })
-        .catch((err) => console.log(err))
-    }
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        navigate("/projects", {
+          state: { message: "Projeto criado com sucesso!" },
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 
-    return (
-        <div className={styles.newproject_container}>
-            <h1>Criar projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm 
-                handleSubmit={createPost} 
-                btnText="Criar projeto" 
-            />
-        </div>
-    )
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
